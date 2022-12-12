3421626c6bac24d040dc91097fe4c16428d7c523
Yunjin Lee <sweejin@naver.com>
2022-12-12 16:22:09 +0000

    build

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={"/wanted-pre-onboarding-frontend"}>
     <App />
   </BrowserRouter>
 );
